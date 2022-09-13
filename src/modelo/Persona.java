f3c081fd261c75332eb8070197a1d28e066f4e78
Carlos Caceres Ochoa <carlos.caceres.mt@usa.edu.co>
2022-09-12 21:42:25 -0500

    prime commit

---
+++ src/modelo/Persona.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author DELL
+ */
+public class Persona {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hola2");
+        System.out.println("hola3");
+        
+    }
+    
+}
